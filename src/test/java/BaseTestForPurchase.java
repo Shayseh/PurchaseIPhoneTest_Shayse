e8d1995c6a39f4a87f9fafc8ceacabc60b70ba72
Shayseh <shayseh@gmail.com>
2026-03-01 16:01:51 -0200

    Added and modified BasicTest Package

--- src/test/java/BaseTestForPurchase.java
+++ src/test/java/BaseTestForPurchase.java
@@ -2,8 +2,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.w3c.dom.Element;
 import org.w3c.dom.html.HTMLInputElement;
 
 import java.time.Duration;
@@ -13,7 +17,7 @@ public class BaseTestForPurchase {
     WebDriver driver;
 
     @Test
-    public void LoginToNdosiWebsite() {
+    public void LoginToNdosiWebsite()  {
 
         driver = new EdgeDriver();
         driver.get("https://ndosisimplifiedautomation.vercel.app/");
@@ -23,11 +27,21 @@ public class BaseTestForPurchase {
         driver.findElement(By.id("login-password")).sendKeys("@11712066");
         driver.findElement(By.id("login-submit")).click();
 
-        new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
-        driver.findElement(By.xpath("//button[contains(@class,'user-pill')]"));
-        HTMLInputElement user = null;
-        user.click();
+        WebElement welcomeText = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(.,'Welcome back')]")));
+
+        Assert.assertTrue(welcomeText.isDisplayed());
+
+        driver.findElement(By.cssSelector(".nav-dropdown-trigger")).click();
+        driver.findElement(By.xpath("//button[contains(.,'Learning Materials')]")).click();
+
+        driver.findElement(By.id("tab-btn-web")).click();
+
+        driver.findElement(By.id("deviceType"));
+        Select device = new Select(driver.findElement(By.id("deviceType")));
+        device.selectByVisibleText("Phone");
 
 
     }
